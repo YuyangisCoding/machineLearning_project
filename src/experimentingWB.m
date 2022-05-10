245558208576b2f164d57d40da56bdbb28fa9e47
BUG MASTER <84109471+YuyangisCoding@users.noreply.github.com>
2022-05-11 09:21:14 +1000

    added

---
+++ src/experimentingWB.m
@@ -0,0 +1,28 @@
+% because filtering symmetric behaviour of neural nets is hard
+% so we sum each row of weightsMatrix instaed
+% networks that are symmetrical to each other should have similar sum
+% then we could do hierarchical clustering and plot dendrogram 
+
+% sum each weights and biases vector
+summation = sum(weightsMatrix, 2);
+
+% sort it 
+sortedSum = sort(summation);
+% dendrogram
+method = "average";
+metric = 'cityblock';
+tree = linkage(sortedSum, method, metric);
+figure
+dendrogram(tree)
+
+% 2d plot
+figure
+plot(sortedSum, LineWidth=2)
+num = size(sortedSum);
+num = num(1);
+% text(1:num, sortedSum, table2cell(array2table(result(:,6))))
+title("plot of summation of weights for each training")
+
+
+
+
