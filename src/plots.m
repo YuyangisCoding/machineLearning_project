07c2ded7cd7a1b5141ce51060c435af9f47764d2
BUG MASTER <84109471+YuyangisCoding@users.noreply.github.com>
2022-06-02 16:10:12 +1000

    updated

--- src/plots.m
+++ src/plots.m
@@ -39,12 +39,12 @@ title(titleContent)
 % dimentianality reduction to 2d and 3d
 
 % colormap
-% num_of_color = size(weightsMatrix);
-% num_of_color = num_of_color(1);
-% cm = jet(num_of_color);
+num_of_color = size(weightsMatrix);
+num_of_color = num_of_color(1);
+cm = jet(num_of_color);
 [coef, score] = pca(weightsMatrix, "Algorithm","eig"); 
 figure
-gscatter(score(:,1), score(:,2), result(:,plot_type));
+gscatter(score(:,1), score(:,2), result(:,plot_type), cm);
 title("pca in 2d")
 
 figure 
@@ -52,14 +52,14 @@ scatter3(score(:,1), score(:,2), score(:,3))
 title("pca in 3d")
 
 % training error rate distribution fiugre
-%figure
-%histogram(result(:,6))
-%title('training error rate distribution')
+figure
+histogram(result(:,6))
+title('training error rate distribution')
 
 % tsne
-figure
-tsne_result = tsne(weightsMatrix,Perplexity=80, Algorithm="exact");
-gscatter(tsne_result(:,1), tsne_result(:,2))
+% figure
+% tsne_result = tsne(weightsMatrix,Perplexity=80, Algorithm="exact");
+% gscatter(tsne_result(:,1), tsne_result(:,2))
 
 
 
